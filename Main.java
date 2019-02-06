a52b632275ef0ea8ccb0381d4f6977bbe706e736
Alexandra Bozzini <alexandrabozzini@zipcodersmbp10.lan>
2019-02-06 12:55:13 -0500

    Finished lab

--- Main.java
+++ Main.java
@@ -8,13 +8,20 @@ public class Main {
     {
         
         
-        Scanner name = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
             System.out.print ("What is your name?");
            
-            String getName = name.nextLine();
-            System.out.print(getName);
+            String name = reader.nextLine();
             
-            if(getName == "Alice") {
+            if(name.equals ("Alice")) {
+                System.out.print ("Hello Alice!");
+            } else if (name.equals( "Bob")){
+                System.out.print ("Hello Bob!");
+            } else {
+                System.out.print ("Goodbye");
+            }
+                
+                
                 
             }
                 
@@ -28,4 +35,4 @@ public class Main {
         
 
     }
-}
+
