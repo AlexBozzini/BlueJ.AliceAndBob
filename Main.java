1023650995ea7800c5ea6aed8e83a2a534017ccc
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-10.local>
2019-02-06 10:30:42 -0500

    Started lab

--- Main.java
+++ Main.java
@@ -1,9 +1,31 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args )
+    {
+        
+        
+        Scanner name = new Scanner(System.in);
+            System.out.print ("What is your name?");
+           
+            String getName = name.nextLine();
+            System.out.print(getName);
+            
+            if(getName == "Alice") {
+                
+            }
+                
+                
+            
+            
+        
+        
+        
+        
+        
 
     }
 }
